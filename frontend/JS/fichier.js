eab81c940cb7cda87f6be5577e2b9c9b2c1472cb
Julie LORIAU <julieloriau@gmail.com>
2020-10-03 16:48:06 +0200

    rétablissement fichier image + commentaires avec formules js envisageables

--- frontend/JS/fichier.js
+++ frontend/JS/fichier.js
@@ -1,33 +1,55 @@
+/*NAVIGATION*/
+$(document).ready(function(){
+    $('.button').click(function(){
+        $('.navbar').toggleClass('isOpen');
+    });
+}); 
+
+
 /*ACCUEIL*/
 function recupBisounours (){
-    var url  = "http://localhost:3000/api/teddies";
-    var request = new XMLHttpRequest()
+    let url  = "http://localhost:3000/api/teddies";
+    let request = new XMLHttpRequest()
     request.open('GET', url, true)
     request.onload = function () {
         var reponse = JSON.parse(request.responseText);
         if (request.readyState == 4 && request.status == "200") {
             $('#result').html('');
-            console.log(reponse);
-            let bear=null;
-            for (var i = 0; i < reponse.length ; i ++){
-                if(reponse[i]["name"]){
-                    bear = $('#contenu').parent().html();
-                    console.log(bear); 
-                    bear=bear.replace("[url]",reponse[i]["imageUrl"]);
-                    bear=bear.replace("[id]",reponse[i]["_id"]);
-                    bear=bear.replace("[title]",reponse[i]["name"]);
-                    console.log(reponse[i]["name"]);
-                    bear=bear.replace("[description]",reponse[i]["description"]);
-                    $('#result').append(bear);
-                    console.log(bear);
-                }   
+                console.log(reponse);
+                for (let i = 0; i < reponse.length ; i ++){
+                    if (reponse[i]["name"]){
+                        console.log(reponse[i]["name"]);
+                        let bear = document.getElementById("contenu").innerHTML;
+                        console.log(bear); 
+                        bear=bear.replace("[url]",reponse[i]["imageUrl"]);
+                        let urlBase = url + reponse[i]["_id"]; 
+                        bear=bear.replace("[id]",urlBase);
+                        bear=bear.replace("[title]",reponse[i]["name"]);
+                        console.log(reponse[i]["name"]);
+                        bear=bear.replace("[description]",reponse[i]["description"]);
+                        $('#result').append(bear);
+                        console.log(bear);
+                    }   
             }
         } else {
             console.error(reponse);
         }
     }
     request.send(null);
-}
+};
+
+/*Clic sur le bouton du produit*/
+let boutonElt = document.getElementById("bouton");
+boutonElt.addEventListener("click", function () {
+    console.log("clic");
+});
+
+//essai pour remplacement url à insérer après ligne 15
+///var urlbase = reponse[i]["imageUrl"];   
+
+//conservation ligne 16 initiale 
+///bear=bear.replace("[url]",reponse[i]["imageUrl"]);
+
 
 /*autre formule à partir cours
 function recupBisounours (){
@@ -54,25 +76,57 @@ function recupBisounours (){
                 console.error(reponse);
             }
     }    
-    };
+};
     request.open("GET", "http://localhost:3000/api/teddies");
     request.send();
+}
 */
+//affichage pour tout le tableau
+let tab = ["Norbert", "Gustav", "Garfunkel", "Lenny and Carl", "Arnold"];
+for (let i=0; i<tab.length;i+=1){
+    console.log(tab[i]);
+};
+
+//Essayer de récuperer depuis le tableau pour affichage
+for (let i=0; i<colors.length;i+=1){
+    console.log(tab[i]);
+};
+
+// variable bouton augmenter quantité
+for (let i = 0; i<99; i+=1);{
+
+}
+
+// variable bouton augmenter quantité
+for (let i = 0; i>-1; i-=1);{
+
+}
 
 /* Pour l'ajout dans le panier 
 localStorage.setItem("", JSON.stringify(.....));
 */
+/* AJOUT D'UN PRODUIT AU PANIER
+function ajout (){
+    let productNumber = localStorage.getItems('ajout');
+    if (productNumber) {
+        localStorage.setItem ('ajout', value);
+    }
+    else{
+        localStorage.setItem ('ajout', keyvalue);
+    }
+}*/
 
 /*PRODUITS*/
+//////affichage des cards /////
 function recupBisounoursProduits (){
-    var url  = "http://localhost:3000/api/teddies";
-    var request = new XMLHttpRequest()
+    let url  = "http://localhost:3000/api/teddies";
+    let request = new XMLHttpRequest()
     request.open('GET', url, true)
     request.onload = function () {
-        var reponse = JSON.parse(request.responseText);
+        let reponse = JSON.parse(request.responseText);
         if (request.readyState == 4 && request.status == "200") {
-            /*console.log(reponse);*/
-            for (var i = 0; i < reponse.length ; i ++){
+            console.log(reponse);
+            for (let i = 0; i < reponse.length ; i ++){
                 console.log(reponse[i]["imageUrl"])
                 console.log(reponse[i]["colors"])
                 console.log(reponse[i]["description"])
@@ -85,17 +139,27 @@ function recupBisounoursProduits (){
     }
     request.send(null);
 }
+/*src https://openclassrooms.com/fr/courses/3306901-creez-des-pages-web-interactives-avec-javascript/3722361-utilisez-des-api-web*/
+/*var image = document.getElementById("img");
+ajaxGet("http://localhost:3000/api/teddies",
+function (reponse) {
+    var teddy = JSON.parse(reponse);
+    var imageUrl.elt = document.createElement("img");
+    imageUrl.src = image.imageUrl;
+    image.appendChild(imageTeddy);
+});
+*/
 
 /*PANIER*/
 function recupBisounoursPanier (){
-    var url  = "http://localhost:3000/api/teddies";
-    var request = new XMLHttpRequest()
+    let url  = "http://localhost:3000/api/teddies";
+    let request = new XMLHttpRequest()
     request.open('GET', url, true)
     request.onload = function () {
-        var reponse = JSON.parse(request.responseText);
+        let reponse = JSON.parse(request.responseText);
         if (request.readyState == 4 && request.status == "200") {
-            /*console.log(reponse);*/
-            for (var i = 0; i < reponse.length ; i ++){
+            console.log(reponse);
+            for (let i = 0; i < reponse.length ; i ++){
                 console.log(reponse[i]["imageUrl"])
                 console.log(reponse[i]["colors"])
                 console.log(reponse[i]["name"])                
@@ -107,9 +171,8 @@ function recupBisounoursPanier (){
     request.send(null);
 }
 
-/* vérification données formulaires
-var request = new XMLHttpRequest();
-request.open("POST", "url à définir");
+/* vérification données formulaires*/
+let request = new XMLHttpRequest();
+request.open("POST", url + "/order");
 request.setRequestHeader("Content-Type", "application/json");
 request.send(JSON.stringify(jsonBody));
-*/
